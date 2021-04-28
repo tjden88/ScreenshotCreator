71d60986324cc41974f83ca1517fa9063e6639cc
Денис (work) <dultsevdv@gmail.com>
2021-04-29 04:22:30 +0500

    Добавьте файлы проекта.

---
+++ ScreenshotCreator/ScreenshotHost.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenshotCreator
+{
+    public class ScreenshotHost : IDisposable
+    {
+        /// <summary>
+        /// Начальная прозрачность
+        /// </summary>
+        public double Opacity { get; set; } = 0.5;
+
+        /// <summary>
+        /// Цвет заливки
+        /// </summary>
+        public Color Background { get; set; } = Color.Gray;
+
+        private PictureBox frame;
+
+        private Form screenshotHost;
+
+        private Point p1, p2;
+
+        public Bitmap GetScreenshot()
+        {
+            frame = new()
+            {
+                BackColor = Color.Yellow,
+                BorderStyle = BorderStyle.FixedSingle,
+                Visible = false
+            };
+            Rectangle DispRect = new();
+            foreach (var Scr in Screen.AllScreens)
+            {
+                DispRect.X = Math.Min(DispRect.X, Scr.Bounds.X);
+                DispRect.Y = Math.Min(DispRect.Y, Scr.Bounds.Y);
+                DispRect.Width += Scr.Bounds.Width;
+                DispRect.Height = Math.Max(DispRect.Height, Scr.Bounds.Height);
+            }
+            screenshotHost = new()
+            {
+                ShowInTaskbar = false,
+                FormBorderStyle = FormBorderStyle.None,
+                TopMost = true,
+                Opacity = Opacity,
+                BackColor = Background,
+                TransparencyKey = Color.Yellow,
+                Cursor = Cursors.Cross,
+                Location=DispRect.Location,
+                Size = DispRect.Size
+            };
+            screenshotHost.Controls.Add(frame);
+            screenshotHost.MouseDown += ScreenshotHost_MouseDown;
+            screenshotHost.MouseMove += ScreenshotHost_MouseMove;
+            screenshotHost.MouseUp += ScreenshotHost_MouseUp;
+            screenshotHost.KeyUp += ScreenshotHost_KeyUp; ;
+
+            if (screenshotHost.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    screenshotHost.Opacity = 0;
+                    frame.BorderStyle = BorderStyle.None;
+                    Bitmap bmp = new(frame.Width, frame.Height);
+                    Graphics g = Graphics.FromImage(bmp);
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                    g.CopyFromScreen(screenshotHost.Left + frame.Left, screenshotHost.Top + frame.Top, 0, 0, frame.Size);
+                    return bmp;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+
+        public async Task<Bitmap> GetScreenshotAsync()
+        {
+            return await Task.Run(() => GetScreenshot());
+        }
+
+
+        private void ScreenshotHost_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                screenshotHost.DialogResult = DialogResult.Cancel;
+        }
+
+        private void ScreenshotHost_MouseDown(object sender, MouseEventArgs e)
+        {
+            frame.Visible = true;
+            p1 = e.Location;
+        }
+
+        private void ScreenshotHost_MouseMove(object sender, MouseEventArgs e)
+        {
+            p2 = e.Location;
+            frame.Location = new Point(Math.Min(p1.X, p2.X), Math.Min(p1.Y, p2.Y));
+            frame.Size = new Size(Math.Max(p1.X, p2.X) - frame.Location.X, Math.Max(p1.Y, p2.Y) - frame.Location.Y);
+
+        }
+
+        private void ScreenshotHost_MouseUp(object sender, MouseEventArgs e)
+        {
+            screenshotHost.DialogResult = DialogResult.OK;
+        }
+
+        public void Dispose()
+        {
+            frame = null;
+            screenshotHost.Dispose();
+            screenshotHost = null;
+        }
+    }
+}
---
+++ TestWinForms/Form1.cs
@@ -0,0 +1,22 @@
+﻿using ScreenshotCreator;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TestWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using ScreenshotHost host = new();
+            Bitmap bmp = host.GetScreenshot();
+            pictureBox1.Image = bmp;
+        }
+    }
+}
