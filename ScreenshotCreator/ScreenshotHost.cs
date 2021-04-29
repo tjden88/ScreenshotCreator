8676c9b19220dc7aede2dd4cf847b695ee1204b4
Денис <dultsevdv@gmail.com>
2021-04-29 22:34:53 +0500

    Доработка функциональности, фокус при появлении. Методы взятия скриншота всего окна

--- ScreenshotCreator/ScreenshotHost.cs
+++ ScreenshotCreator/ScreenshotHost.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -7,6 +8,17 @@ namespace ScreenshotCreator
 {
     public class ScreenshotHost : IDisposable
     {
+        #region PrivateFields
+        private PictureBox _Frame;
+
+        private Form _ScreenshotHost;
+
+        private Point p1, p2;
+
+        private bool isDrawing;
+        #endregion
+
+        #region Properties
         /// <summary>
         /// Начальная прозрачность
         /// </summary>
@@ -17,61 +29,86 @@ namespace ScreenshotCreator
         /// </summary>
         public Color Background { get; set; } = Color.Gray;
 
-        private PictureBox frame;
+        /// <summary>
+        /// Ключ прозрачности
+        /// </summary>
+        public Color TransparancyKeyColor { get; set; } = Color.Yellow;
+        #endregion
 
-        private Form screenshotHost;
+        #region ScreenshotMethods
 
-        private Point p1, p2;
+        /// <summary>
+        /// Получить границы всех экранов
+        /// </summary>
+        public static Rectangle GetBounds()
+        {
+            Rectangle dispRect = new();
+            foreach (var scr in Screen.AllScreens)
+            {
+                dispRect.X = Math.Min(dispRect.X, scr.Bounds.X);
+                dispRect.Y = Math.Min(dispRect.Y, scr.Bounds.Y);
+                dispRect.Width += scr.Bounds.Width;
+                dispRect.Height = Math.Max(dispRect.Height, scr.Bounds.Height);
+            }
+            return dispRect;
+        }
 
-        private bool isDrawing;
-
+        /// <summary>
+        /// Скриншот области экрана
+        /// </summary>
         public Bitmap GetScreenshot()
         {
-            frame = new()
+            _Frame = new()
             {
-                BackColor = Color.Yellow,
+                BackColor = TransparancyKeyColor,
                 BorderStyle = BorderStyle.FixedSingle,
-                Size = new Size(0,0),
+                Size = new Size(0, 0),
                 Visible = false
             };
-            Rectangle DispRect = new();
-            foreach (var Scr in Screen.AllScreens)
-            {
-                DispRect.X = Math.Min(DispRect.X, Scr.Bounds.X);
-                DispRect.Y = Math.Min(DispRect.Y, Scr.Bounds.Y);
-                DispRect.Width += Scr.Bounds.Width;
-                DispRect.Height = Math.Max(DispRect.Height, Scr.Bounds.Height);
-            }
-            screenshotHost = new()
+
+            Rectangle dispRect = GetBounds();
+            _ScreenshotHost = new()
             {
                 ShowInTaskbar = false,
                 FormBorderStyle = FormBorderStyle.None,
-                TopMost = true,
                 Opacity = Opacity,
                 BackColor = Background,
-                TransparencyKey = Color.Yellow,
-                Cursor = Cursors.Cross,
-                Location=DispRect.Location,
-                Size = DispRect.Size
+                TransparencyKey = TransparancyKeyColor,
+                KeyPreview = true,
+                Cursor = Cursors.Cross
             };
-            screenshotHost.Controls.Add(frame);
-            screenshotHost.MouseDown += ScreenshotHost_MouseDown;
-            screenshotHost.MouseMove += ScreenshotHost_MouseMove;
-            screenshotHost.MouseUp += ScreenshotHost_MouseUp;
-            screenshotHost.KeyUp += ScreenshotHost_KeyUp;
-            screenshotHost.Activated += (_, _) => screenshotHost.Focus();
+            _ScreenshotHost.Controls.Add(_Frame);
+            _ScreenshotHost.MouseDown += ScreenshotHost_MouseDown;
+            _ScreenshotHost.MouseMove += ScreenshotHost_MouseMove;
+            _ScreenshotHost.MouseUp += ScreenshotHost_MouseUp;
+            _ScreenshotHost.KeyUp += ScreenshotHost_KeyUp;
+            _ScreenshotHost.Load += (_, _) =>
+            {
+                _ScreenshotHost.Location = dispRect.Location;
+                _ScreenshotHost.Size = dispRect.Size;
+                _ScreenshotHost.TopMost = true;
+            };
 
-            if (screenshotHost.ShowDialog() == DialogResult.OK)
+            _ScreenshotHost.Activated += (_, _) =>
+            {
+                _ScreenshotHost.BringToFront();
+                _ScreenshotHost.Select();
+                _ScreenshotHost.Focus();
+                Debug.WriteLine("Activated");
+            };
+            _ScreenshotHost.Deactivate += (_, _) => Debug.WriteLine("Deactivated");
+
+            if (_ScreenshotHost.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
-                    screenshotHost.Opacity = 0;
-                    frame.BorderStyle = BorderStyle.None;
-                    Bitmap bmp = new(frame.Width, frame.Height);
+                    _ScreenshotHost.Opacity = 0;
+                    _Frame.BorderStyle = BorderStyle.None;
+                    Bitmap bmp = new(_Frame.Width, _Frame.Height);
                     Graphics g = Graphics.FromImage(bmp);
                     g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
                     g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-                    g.CopyFromScreen(screenshotHost.Left + frame.Left, screenshotHost.Top + frame.Top, 0, 0, frame.Size);
+                    g.CopyFromScreen(_ScreenshotHost.Left + _Frame.Left, _ScreenshotHost.Top + _Frame.Top, 0, 0, _Frame.Size);
                     return bmp;
                 }
                 catch (Exception)
@@ -82,24 +119,51 @@ namespace ScreenshotCreator
             return null;
         }
 
-        public async Task<Bitmap> GetScreenshotAsync()
+        /// <summary>
+        /// Скриншот всех экранов
+        /// </summary>
+        public Bitmap GetFullscreenScreenshot()
         {
-            return await Task.Run(() => GetScreenshot());
+            Rectangle dispRect = GetBounds();
+            try
+            {
+                Bitmap bmp = new(dispRect.Width, dispRect.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.CopyFromScreen(dispRect.Left, dispRect.Top, 0, 0, dispRect.Size);
+                return bmp;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
+        
+        /// <summary>
+        /// Скриншот области экрана
+        /// </summary>
+        public async Task<Bitmap> GetScreenshotAsync() => await Task.Run(GetScreenshot);
 
+        /// <summary>
+        /// Скриншот всех экранов
+        /// </summary>
+        public async Task<Bitmap> GetFullscreenScreenshotAsync() => await Task.Run(GetFullscreenScreenshot);
 
+        #endregion
+
+        #region Mouse
         private void ScreenshotHost_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Escape)
-                screenshotHost.DialogResult = DialogResult.Cancel;
+                _ScreenshotHost.DialogResult = DialogResult.Cancel;
         }
-
         private void ScreenshotHost_MouseDown(object sender, MouseEventArgs e)
         {
-            frame.Location = e.Location;
+            _Frame.Location = e.Location;
             p1 = e.Location;
             p2 = e.Location;
-            frame.Visible = true;
+            _Frame.Visible = true;
             isDrawing = true;
         }
 
@@ -107,21 +171,23 @@ namespace ScreenshotCreator
         {
             if (!isDrawing) return;
             p2 = e.Location;
-            frame.Location = new Point(Math.Min(p1.X, p2.X), Math.Min(p1.Y, p2.Y));
-            frame.Size = new Size(Math.Max(p1.X, p2.X) - frame.Location.X, Math.Max(p1.Y, p2.Y) - frame.Location.Y);
+            _Frame.Location = new Point(Math.Min(p1.X, p2.X), Math.Min(p1.Y, p2.Y));
+            _Frame.Size = new Size(Math.Max(p1.X, p2.X) - _Frame.Location.X, Math.Max(p1.Y, p2.Y) - _Frame.Location.Y);
 
         }
 
         private void ScreenshotHost_MouseUp(object sender, MouseEventArgs e)
         {
-            screenshotHost.DialogResult = DialogResult.OK;
-        }
+            _ScreenshotHost.DialogResult = DialogResult.OK;
+        } 
+        #endregion
 
         public void Dispose()
         {
-            frame = null;
-            screenshotHost.Dispose();
-            screenshotHost = null;
+            _Frame = null;
+            _ScreenshotHost?.Dispose();
+            _ScreenshotHost = null;
+            GC.SuppressFinalize(this);
         }
     }
 }
--- TestWinForms/Form1.cs
+++ TestWinForms/Form1.cs
@@ -15,8 +15,33 @@ namespace TestWinForms
         private void button1_Click(object sender, EventArgs e)
         {
             using ScreenshotHost host = new();
+            Hide();
             Bitmap bmp = host.GetScreenshot();
             pictureBox1.Image = bmp;
+            Show();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            using ScreenshotHost host = new();
+            WindowState = FormWindowState.Minimized;
+            Bitmap bmp = await host.GetScreenshotAsync();
+            pictureBox1.Image = bmp;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using ScreenshotHost host = new();
+            pictureBox1.Image = host.GetFullscreenScreenshot();
+
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            using ScreenshotHost host = new();
+            pictureBox1.Image = await host.GetFullscreenScreenshotAsync();
+
         }
     }
 }
